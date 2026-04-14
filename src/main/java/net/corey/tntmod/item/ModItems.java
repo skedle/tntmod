59e919b28fda7b79e30c4eb2944f8a3c63b6ee7c
Corey <smithc5907@gmail.com>
2026-04-14 00:45:00 -0400

    camellia

--- src/main/java/net/corey/tntmod/item/ModItems.java
+++ src/main/java/net/corey/tntmod/item/ModItems.java
@@ -33,6 +33,9 @@ public class ModItems {
     public static final RegistryObject<Item> TOADSTONE = ITEMS.register("toadstone",
             () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> CAMELLIA = ITEMS.register("camellia",
+            () -> new Item(new Item.Properties()));
+
     // --------------------------------------------------------------------------------------------------------------------------------
     // Food
 
